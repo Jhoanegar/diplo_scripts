3e2eb8877810b0668063c8e946b2690b31a0ca7a
Jhoan Garcia <jhoanegar@gmail.com>
2021-04-17 11:17:08 -0400

    Redirect ouptup of create_users to dev null

--- create_users.sh
+++ create_users.sh
@@ -21,12 +21,12 @@ users=`cat $1`
 for user in ${users//,/ }
 do
     echo "Creating user $user..."
-    `adduser $user`
+    adduser $user > /dev/null
     echo "User $user was created successfully"
 
-    echo "diplomado12" | passwd --stdin $user
+    echo "diplomado12" | passwd --stdin $user > /dev/null
     echo "Default password for $user was set successfully"
 
-    passwd --expire $user
-    echo "Done!"
+    passwd --expire $user > /dev/null
+    echo -e "Done!\n"
 done
